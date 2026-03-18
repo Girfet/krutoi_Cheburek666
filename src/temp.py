e98ce301e9b2e5b732c80e125a7b403f7a0bf249
Girfet <girfet@mail.ru>
2026-03-18 16:44:34 +0300

    Update temp.py

--- src/temp.py
+++ src/temp.py
@@ -1,8 +1,30 @@
-# n = 0b101
-# print(f"n={n}")
+fridge = {
+    "долгопортищиеся": {
+        "консервы": ["Лосось", "килька"],
+        "мёд": ["Пчёлка", "Сытый медведь"]
+    },
+    "скоропортищиеся": {
+        "кисломолочные": ["Молоко", "Творог"],
+        "мясное": ["копчёнка", "Курица"]
+        }
+}   
+print(fridge)
+
+def add_item(data):
+    if not data:
+        print("Сначала создайте хотя бы одну категорию.")
+        return
+    
+    print("\nДоступные категории:")
+
+    categories = list(data.keys())
+    for i in range(len(categories)):
+        print(f"  {i + 1}. {categories[i]}")
 
 
-numbers = []
-for index in range(3):
-    numbers.append(input('Введите слова Любое которое придёт в бошку'))
-print(numbers)
+
+
+
+
+
+add_item(fridge)
